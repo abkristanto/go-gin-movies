5f107be559fa04e20eb21d0878be40f90381ee13
Yusuf Abraham Bismo Kristanto <yusuf.kristanto@shopee.com>
2022-10-10 02:49:08 +0700

    feat: add grouping

--- main.go
+++ main.go
@@ -10,15 +10,22 @@ import (
 func main() {
 	r := gin.Default()
 	h := handlers.InitHandler()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	api := r.Group("/api")
+	{
+		api.GET("/ping", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
 		})
-	})
-	r.POST("/movies", h.CreateMovie)
-	r.POST("/register", h.RegisterUser)
-	r.POST("/genres", h.CreateGenre)
-	r.GET("/genres/:id", h.GetGenreById)
+		genres := api.Group("/genres")
+		{
+			genres.POST("/", h.CreateGenre)
+			genres.GET("/:id", h.GetGenreById)
+		}
+		api.POST("/movies", h.CreateMovie)
+		api.DELETE("/movies/:id", h.DeleteMovie)
+		api.POST("/register", h.RegisterUser)
+	}
 	err := r.Run()
 	if err != nil {
 		panic(err)
