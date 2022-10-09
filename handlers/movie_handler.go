176dfe32990c7a64f46168f6b50aa59454a50c56
Yusuf Abraham Bismo Kristanto <yusuf.kristanto@shopee.com>
2022-10-10 02:48:54 +0700

    feat: add delete movie handler

--- handlers/movie_handler.go
+++ handlers/movie_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/abkristanto/go-gin-movies/dtos"
 	"github.com/abkristanto/go-gin-movies/helpers"
@@ -16,7 +17,7 @@ func (h *Handler) CreateMovie(c *gin.Context) {
 		helpers.WriteErrorResponse(c, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
 		return
 	}
-	
+
 	movieResponse, err := h.movieService.CreateMovie(createMovieRequest)
 
 	if err != nil {
@@ -25,4 +26,18 @@ func (h *Handler) CreateMovie(c *gin.Context) {
 	}
 
 	helpers.WriteSuccessResponse(c, movieResponse)
+}
+
+func (h *Handler) DeleteMovie(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		helpers.WriteErrorResponse(c, http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		return
+	}
+	err = h.movieService.DeleteMovie(id)
+	if err != nil {
+		helpers.WriteErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	helpers.WriteSuccessResponse(c, nil)
 }
